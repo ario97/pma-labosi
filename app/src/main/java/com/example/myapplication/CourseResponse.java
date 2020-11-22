39bd86ae9419aa8617d19bdda30aefbf3e47f9fa
ario97 <m1brbot@gmail.com>
2020-11-22 20:45:04 +0100

    nadogradnja na lv5

---
+++ app/src/main/java/com/example/myapplication/CourseResponse.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class CourseResponse {
+    ArrayList<Course>courses;
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+}
